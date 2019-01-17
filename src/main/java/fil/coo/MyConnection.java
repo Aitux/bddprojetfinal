04208ac20b0d9fad388a667bcb929b3441f58fa1
vandeputte <simonvandeputte.etu@univ-lille.fr>
2019-01-17 14:22:01 +0100

    derniere part

---
+++ src/main/java/fil/coo/MyConnection.java
@@ -0,0 +1,39 @@
+package fil.coo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyConnection
+{
+    private static MyConnection ourInstance = new MyConnection();
+
+    public static MyConnection getInstance()
+    {
+        return ourInstance;
+    }
+
+    public Connection getConnection(){
+        try
+        {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection c = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fil.univ-lille1.fr:1521:filora", "devassine", "Alex09011996");
+            return c;
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void close(Connection c){
+        try{
+            c.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private MyConnection()
+    {
+    }
+}
--- src/main/java/fil/coo/PDFBuilder.java
+++ src/main/java/fil/coo/PDFBuilder.java
@@ -8,9 +8,10 @@ import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.util.Date;
 
-public class Main {
+public class PDFBuilder
+{
 
-    private static String FILE = "/home/aitux/Bureau/MiageBook.pdf";
+    private static String FILE = "/home/m1miage/vandeputte/Desktop/MiageBook.pdf";
     private static Font title = new Font(Font.FontFamily.TIMES_ROMAN, 42,
             Font.BOLD);
     private static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
@@ -23,46 +24,62 @@ public class Main {
     private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12,
             Font.BOLD);
 
-    private static String clobToString(Clob data) {
+    private static String clobToString(Clob data)
+    {
         StringBuilder sb = new StringBuilder();
-        try {
+        try
+        {
             Reader reader = data.getCharacterStream();
             BufferedReader br = new BufferedReader(reader);
 
             String line;
-            while (null != (line = br.readLine())) {
+            while (null != (line = br.readLine()))
+            {
                 sb.append(line);
             }
             br.close();
-        } catch (SQLException e) {
-            // handle this exception
-        } catch (IOException e) {
-            // handle this exception
+        } catch (SQLException | IOException e)
+        {
+            // do smth crazy :)
+            e.printStackTrace();
         }
         return sb.toString();
     }
 
-    public static void main(String[] args) {
+    public static void build()
+    {
 
-        try {
+        try
+        {
             Document doc = new Document();
             PdfWriter.getInstance(doc, new FileOutputStream(FILE));
             doc.open();
             addMetaData(doc);
             addTitlePage(doc);
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection c = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fil.univ-lille1.fr:1521:filora", "vandeputte", "e94a0dc724");
-            PreparedStatement stmt = c.prepareStatement("select (cv).getClobVal() as cv from candidats where cv is not null");
-            PreparedStatement id = c.prepareStatement("SELECT idcand from candidats where nom=?");
-            PreparedStatement note = c.prepareStatement("SELECT id_epreuve, note from notes where idcand = ?");
+            Connection c = MyConnection.getInstance().getConnection();
+            PreparedStatement stmt = c.prepareStatement("select photoid, (cv).getClobVal() as cv from candidat where cv is not null AND PHOTOID is not null");
+            PreparedStatement id = c.prepareStatement("SELECT id from candidat where nom=?");
+            PreparedStatement note = c.prepareStatement("SELECT id_epreuve, note from notes where ID_CANDIDAT = ?");
             PreparedStatement voeux = c.prepareStatement("SELECT ide from voeux where idc=?");
             ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
+            while (rs.next())
+            {
                 int idcand = 0;
-                String str = clobToString(rs.getClob(1));
+
+                String str = clobToString(rs.getClob(2));
                 InputStream stream = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
+
                 CV cv = XMLParser.parse(stream);
-                Paragraph page = printCVPage(cv);
+                Paragraph page = new Paragraph();
+                Blob imageBlob = rs.getBlob(1);
+                byte[] imageBytes = imageBlob.getBytes(1, (int) imageBlob.length());
+                Image image = Image.getInstance(imageBytes);
+                image.scaleAbsolute(125, 125);
+                image.setAlignment(Element.ALIGN_RIGHT);
+                page.add(image);
+                Paragraph p = printCVPage(cv);
+                page.add(p);
+
                 String nom = cv.getNom().toLowerCase();
                 char[] stringArray = nom.trim().toCharArray();
                 stringArray[0] = Character.toUpperCase(stringArray[0]);
@@ -74,37 +91,44 @@ public class Main {
                     idcand = rs1.getInt(1);
                 note.setInt(1, idcand);
                 ResultSet rs2 = note.executeQuery();
-                Paragraph p = new Paragraph("Epreuve | Note");
+                p = new Paragraph("Epreuve | Note");
                 page.add(p);
-                while(rs2.next()) {
+                while (rs2.next())
+                {
                     p = new Paragraph(rs2.getInt(1) + " | " + rs2.getDouble(2));
                     page.add(p);
                 }
 
-                voeux.setInt(1,idcand);
+                voeux.setInt(1, idcand);
                 ResultSet rs3 = voeux.executeQuery();
-                addEmptyLine(page,2);
+                addEmptyLine(page, 2);
                 p = new Paragraph("Voeux (par ordre de priorité):");
                 page.add(p);
-                while(rs3.next()){
-                     p = new Paragraph("Ecole " + rs3.getInt(1));
-                     page.add(p);
+                while (rs3.next())
+                {
+                    p = new Paragraph("Ecole " + rs3.getInt(1));
+                    page.add(p);
                 }
 
-                try {
+                try
+                {
                     doc.add(page);
                     doc.newPage();
-                } catch (DocumentException e) {
+                } catch (DocumentException e)
+                {
                     e.printStackTrace();
                 }
             }
+            MyConnection.getInstance().close(c);
             doc.close();
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             e.printStackTrace();
         }
     }
 
-    private static Paragraph printCVPage(CV c) {
+    private static Paragraph printCVPage(CV c)
+    {
         Paragraph page = new Paragraph();
         addEmptyLine(page, 1);
         Paragraph p = new Paragraph(c.getPrenom() + " " + c.getNom(), title);
@@ -120,9 +144,10 @@ public class Main {
         p = new Paragraph("tel: " + c.getTel(), smallBold);
         page.add(p);
         addEmptyLine(page, 2);
-        p = new Paragraph("Technolgies connues: ", smallBold);
+        p = new Paragraph("Technologies connues: ", smallBold);
         for (String str :
-                c.getTechnos()) {
+                c.getTechnos())
+        {
             p.add(str + " | ");
         }
         page.add(p);
@@ -131,7 +156,8 @@ public class Main {
 
     }
 
-    private static void addMetaData(Document document) {
+    private static void addMetaData(Document document)
+    {
         document.addTitle("Miage's CVs Book");
         document.addSubject("TP de conclusion de l'UE BDD");
         document.addKeywords("Java, PDF, iText, BDD, MIAGE");
@@ -140,7 +166,8 @@ public class Main {
     }
 
     private static void addTitlePage(Document document)
-            throws DocumentException {
+            throws DocumentException
+    {
         Paragraph preface = new Paragraph();
         // We add one empty line
         addEmptyLine(preface, 1);
@@ -165,8 +192,10 @@ public class Main {
         document.newPage();
     }
 
-    private static void addEmptyLine(Paragraph paragraph, int number) {
-        for (int i = 0; i < number; i++) {
+    private static void addEmptyLine(Paragraph paragraph, int number)
+    {
+        for (int i = 0; i < number; i++)
+        {
             paragraph.add(new Paragraph(" "));
         }
     }
